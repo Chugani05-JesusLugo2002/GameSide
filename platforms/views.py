96300f59cf4b941d75707f46cb323195493c5f62
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-02-10 09:43:31 +0000

    Refactor platforms app

--- platforms/views.py
+++ platforms/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest, JsonResponse
+
 from shared.decorators import assert_method, assert_object_found
 
 from .models import Platform
@@ -5,7 +7,7 @@ from .serializers import PlatformSerializer
 
 
 @assert_method('GET')
-def platform_list(request):
+def platform_list(request: HttpRequest) -> JsonResponse:
     platforms = Platform.objects.all()
     serializer = PlatformSerializer(platforms, request=request)
     return serializer.json_response()
@@ -13,7 +15,7 @@ def platform_list(request):
 
 @assert_method('GET')
 @assert_object_found(Platform, with_slug=True)
-def platform_detail(request, platform_slug):
+def platform_detail(request: HttpRequest, platform_slug: str) -> JsonResponse:
     platform = Platform.objects.get(slug=platform_slug)
     serializer = PlatformSerializer(platform, request=request)
     return serializer.json_response()
